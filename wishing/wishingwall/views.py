4edad487c8bc6ddb094a476514fcd959bde5ff09
Ccl <540046768@qq.com>
2016-04-27 18:28:33 +0800

    update
    tm，这破玩意的 ignore 列表没啥用啊

--- wishing/wishingwall/views.py
+++ wishing/wishingwall/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+from .models import Wishing
 
 def index(request):
+    if request.method == 'POST':
+        # 有POST请求
+        text = request.POST['wishingText']
+        print(text)
+
+        return
     return render(request,'index.html')

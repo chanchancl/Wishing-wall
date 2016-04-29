bf0e633d23d79cd030f79713f50bf9146710145a
Ccl <540046768@qq.com>
2016-04-29 09:27:53 +0800

    心好累
    数据库真麻烦。。没啥解决方法，
    Model 也没有指明如何相应 创建/删除动作
    不然的话，在admin的后台的操作，并不会影响ServerData。。。。

--- wishing/wishingwall/admin.py
+++ wishing/wishingwall/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Wishing,ServerData
+
+admin.site.register(ServerData)
+admin.site.register(Wishing)
--- wishing/wishingwall/models.py
+++ wishing/wishingwall/models.py
@@ -2,6 +2,43 @@ from django.db import models
 
 # Create your models here.
 
+class ServerData(models.Model):
+    totalWishing = models.IntegerField()
+    
+    
+def GetTotalObj():
+    try:
+        obj = ServerData.objects.get(pk=1)
+    except:
+        obj = ServerData.objects.create(totalWishing=1)
+        obj.totalWishing = 0
+    return obj
+    
+def GetCurrentID():
+    return GetTotalObj().totalWishing
+    
+def ModifyTotal(num=1):
+    obj = GetTotalObj()
+    obj.totalWishing += num
+    obj.save()
+    return obj.totalWishing
+    
 class Wishing(models.Model):
     wID = models.IntegerField()
     wText = models.CharField(max_length=100)
+    
+    def create(self,text,*args, **kwargs):
+        id = ModifyTotal()
+        obj = super().create(wID=id,wText=text,*arg,**kwargs)
+        return obj
+        
+    def delete(self, using=None, keep_parents=False):
+        if GetCurrentID() >= 1:
+            ModifyTotal(-1)
+        super().delete()
+        print('-*-*-*-*-*-*-*-delete-*-*-*-*-*-*-*-')
+    
+    def __str__(self):
+        return '%d : %s' %(self.wID,self.wText)
+        
+        
